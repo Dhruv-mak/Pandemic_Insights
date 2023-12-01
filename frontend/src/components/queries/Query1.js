909c23eea8e249edca837c718fbe271d68cc6d1c
Vishal <iamvishalprakash@gmail.com>
2023-12-01 01:56:18 -0500

    Date component added in Query 1 page

--- frontend/src/components/queries/Query1.js
+++ frontend/src/components/queries/Query1.js
@@ -2,8 +2,9 @@ import React from "react";
 import DropdownCheckbox from "../DropdownCheckbox";
 import { get_coutry_list } from "../../services/api";
 import { get_query } from "../../services/api";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
+import DatePicker from "../DatePicker";
 
 const Query1 = () => {
   const [checkedValues, setCheckedValues] = useState([]);
@@ -44,20 +45,33 @@ const Query1 = () => {
     };
   });
   return (
-    <div className="Visual mx-28">
-      <div className="ml-28 mt-10">
-        <DropdownCheckbox
-          buttonText="Select a Country"
-          checkboxItems={checkboxItems}
-          handleCheck={handleCheck}
-        />
+    <div className="Visual mx-28 ">
+      <div className="flex">
+        <div className="inlineml-28 mt-10">
+          <DropdownCheckbox
+            buttonText="Select a Country"
+            checkboxItems={checkboxItems}
+            handleCheck={handleCheck}
+          />
+        </div>
+
+        <div className="inline ml-12 mt-8 pt-2">
+          <DatePicker />
+        </div>
       </div>
       <div className="flex flex-col">
         {queryGraph.map((graph, index) => (
-          <div className="mx-auto mt-5 rounded-2xl hover:shadow-2xl overflow-hidden" key={index}>
-            <Plot 
-              data={graph.data} 
-              layout={{...graph.layout, width: window.innerWidth * 0.65, height: window.innerHeight * 0.7}}
+          <div
+            className="mx-auto mt-5 rounded-2xl hover:shadow-2xl overflow-hidden"
+            key={index}
+          >
+            <Plot
+              data={graph.data}
+              layout={{
+                ...graph.layout,
+                width: window.innerWidth * 0.65,
+                height: window.innerHeight * 0.7,
+              }}
             />
           </div>
         ))}
